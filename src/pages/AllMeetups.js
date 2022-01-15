63b2252635091c898ab97b6f294ae8f7fd565718
Suxrob9966 <rabbimovich9966@gmail.com>
2022-01-15 12:33:32 -0600

    adding routing

---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AllMeetupsPage = () => {
+  return <div>All Meetups page</div>;
+};
+
+export default AllMeetupsPage;
---
+++ src/pages/NewMeetups.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NewMeetupsPage = () => {
+  return <div>New Meetups page</div>;
+};
+
+export default NewMeetupsPage;
