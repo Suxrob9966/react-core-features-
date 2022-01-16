bfc6f2fa4bf078b0f9e236b3e36277369347132c
Suxrob9966 <rabbimovich9966@gmail.com>
2022-01-16 15:56:37 -0600

    sending a POST request to Firebase

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -2,7 +2,18 @@ import React from "react";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetupsPage = () => {
-  const addMeetupHandler = (meetupData) => {};
+  const addMeetupHandler = (meetupData) => {
+    // 1. After slash we can write anything (meetups) that will create that folder with that name in Firebase. 2. URL needs .json at the end.
+    // 3. By default fetch sends a GET request
+    fetch(
+      "https://react-getting-started-b59fa-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  };
 
   return (
     <section>
