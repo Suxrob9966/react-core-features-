1e823498f2997f4b1d1ace8f9b9204b5f3b0d221
Suxrob9966 <rabbimovich9966@gmail.com>
2022-01-16 10:08:52 -0600

    adding more react components

---
+++ src/components/meetups/MeetupList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import MeetupUtem from "./MeetupItem";
+import classes from "./MeetupList.module.css";
+
+const MeetupList = (props) => {
+  return (
+    <ul className={classes.list}>
+      {props.meetups.map((meetup) => (
+        <MeetupUtem
+          key={meetup.id}
+          id={meetup.id}
+          image={meetup.image}
+          title={meetup.title}
+          address={meetup.address}
+          description={meetup.description}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default MeetupList;
