c7f5f796db4682eb2eed8df7893853b26a028ea9
Suxrob9966 <rabbimovich9966@gmail.com>
2022-01-17 10:25:08 -0600

    using useEffect() hook

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 // const DUMMY_DATA = [
@@ -26,16 +26,30 @@ const AllMeetupsPage = () => {
   const [isLoading, setIsLoading] = useState(true); // needed to show the list or the spiinner
   const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-  fetch(
-    "https://react-getting-started-b59fa-default-rtdb.firebaseio.com/meetups.json"
-  )
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      setIsLoading(false);
-      setLoadedMeetups(data);
-    });
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-getting-started-b59fa-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = []; // we need array for outputting with map() in MeetupList com-t.
+        // the data fetched from Firebase is object that is why we need to store each object item in meetups array
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
 
   if (isLoading) {
     return (
